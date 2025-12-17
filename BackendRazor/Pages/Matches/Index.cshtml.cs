2e4280bb078dd07f532a4c93319b336cc3040e89
Niky <nicolae.hotescu23@gmail.com>
2025-12-17 10:32:15 +0200

    test

--- BackendRazor/Pages/Matches/Index.cshtml.cs
+++ BackendRazor/Pages/Matches/Index.cshtml.cs
@@ -1,12 +1,42 @@
-using Microsoft.AspNetCore.Mvc;
+using BackendRazor.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyApp.Pages.Matches
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly FrbContext _context;
+
+        public IndexModel(FrbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Match> UpcomingMatches { get; set; } = new List<Match>();
+        public IList<Match> CompletedMatches { get; set; } = new List<Match>();
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Matches != null)
+            {
+                var allMatches = await _context.Matches
+                    .Include(m => m.IdhomeNavigation) 
+                    .Include(m => m.IdguestNavigation) 
+                    .OrderBy(m => m.Date) 
+                    .ToListAsync();
+
+                UpcomingMatches = allMatches
+                    .Where(m => m.Date.ToDateTime(TimeOnly.MinValue) >= DateTime.Today && (m.Scorehome == null || m.Scoreguest == null))
+                    .ToList();
+
+                CompletedMatches = allMatches
+                    .Where(m => m.Date.ToDateTime(TimeOnly.MinValue) < DateTime.Today || (m.Scorehome != null && m.Scoreguest != null))
+                    .ToList();
+            }
         }
     }
 }
